58feec1d413f066b6b0025d506020a863fa035aa
Sibongile <sibongile.nhlema@gmail.com>
2024-04-24 14:33:44 +0200

    implemeted the contolller for the books model

--- src/routes/authors.js
+++ src/routes/authors.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const router = express.Router();
-const authorController = require('../controllers/authorController');
+const authorController = require('../controllers/author');
 
 router.post('/', authorController.createAuthor);
 router.get('/:id', authorController.getAuthorById);

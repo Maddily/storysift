f5492b74e902fe4717a9e7d3d25947edfccb79d3
Maddie <115709272+Maddily@users.noreply.github.com>
2024-04-25 08:41:47 +0200

    Add route to check authentication status

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user');
+const authMiddleware = require('../middleware/auth');
 
 router.post('/signup', userController.createUser);
 router.post('/signin', userController.signInUser);
@@ -11,5 +12,8 @@ router.get('/:id', userController.getUserById);
 router.put('/:id', userController.updateUser);
 router.delete('/:id', userController.deleteUser);
 router.get('/profile', userController.getUserProfile);
+router.get('/check-authentication', authMiddleware, (req, res) => {
+  res.json({ authenticated: true, user: req.session.user });
+});
 
 module.exports = router;

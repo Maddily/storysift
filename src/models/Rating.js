95407b110665a49a0d61f95b1251794d54349005
Sibongile <sibongile.nhlema@gmail.com>
2024-04-27 23:36:30 +0200

    updated the models schema

--- src/models/Rating.js
+++ src/models/Rating.js
@@ -3,24 +3,19 @@
 const mongoose = require('mongoose');
 
 const ratingSchema = new mongoose.Schema({
-  user_id: {
+  userId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User', // Reference to User model
     required: true
   },
-  author_id: {
+  authorId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Author', // Reference to Author Model
     required: true
   },
-  book_id: {
+  bookId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Book', // Reference to Book model
-    required: true
-  },
-  genre_id: {
-    type: mongoose.Schema.Types.ObjectID,
-    ref: 'Genre', // Reference to Genre model
     required: true
   },
   rating: {

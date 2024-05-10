736fcd21e64a0f6dd9919050168911413d06e5fa
Sibongile <sibongile.nhlema@gmail.com>
2024-05-10 19:45:05 +0200

    Add comments to books routes in src/routes/books.js

--- src/routes/books.js
+++ src/routes/books.js
@@ -4,6 +4,8 @@ const express = require('express');
 const router = express.Router();
 const booksController = require('../controllers/book');
 
+// Define routes
+
 // Route to search books using Google Books API
 router.get('/search', booksController.searchBooks);
 // Route to get book details by volumeId
@@ -15,4 +17,5 @@ router.get('/:id', booksController.getBookById);
 router.put('/:id', booksController.updateBook);
 router.delete('/:id', booksController.deleteBook);
 
+// Export the router
 module.exports = router;

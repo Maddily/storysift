0b76ad6e71a8b05421e7469f4aa11c85ed949101
Sibongile <sibongile.nhlema@gmail.com>
2024-05-10 19:48:49 +0200

    Add comments to searchhistories routes in src/routes/searchhistories.js

--- src/routes/searchHistories.js
+++ src/routes/searchHistories.js
@@ -4,9 +4,11 @@ const express = require('express');
 const router = express.Router();
 const searchHistoryController = require('../controllers/searchHistory');
 
+// Define routes
 router.post('/', searchHistoryController.createSearchHistory);
 router.get('/', searchHistoryController.getAllSearchHistories);
 router.get('/user/:user_id', searchHistoryController.getSearchHistoriesByUserId);
 router.delete('/:id', searchHistoryController.deleteSearchHistory);
 
+// Export the router
 module.exports = router;
